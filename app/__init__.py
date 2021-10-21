3117452978ee069945a0bab6d783bf18e2d3c1df
sourabhpoddar404 <sourabh.poddar404@gmail.com>
2021-10-21 17:01:19 +0200

    app

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
+import json
 from flask import Flask
 import main
 app =  Flask(__name__)
 
 @app.route("/")
 def hello():
-    print(main.evaluate(["What is the capital of India"]))
-    return "Hello World"
+    result = main.evaluate(["What is the capital of India"])
+    relations = result[2]
+    response = {
+        "relations_dbpedia" : relations
+    }
+    resposnejson = json.loads(response)
+    return resposnejson
 
